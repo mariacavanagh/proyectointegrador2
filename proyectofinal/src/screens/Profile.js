4634b62378295f901ea14ea0da5e1ea9f1fe0ddb
LorenzoFernandezA <lfernandezarancibia@udesa.edu.ar>
2025-11-09 19:03:50 -0300

    Datos del Usuario en Profile

--- proyectofinal/src/screens/Profile.js
+++ proyectofinal/src/screens/Profile.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { Component } from "react";
 import { Pressable } from "react-native";
 import { View, Text } from "react-native-web";
+import { auth, db } from '../firebase/config';
 
-function Profile(){
-    return (
+class Profile extends Component{
+    constructor(props){
+    super(props)
+    this.state = {
+      username: '',
+      email: '',
+      misPosts: [],
+    }}
+
+    componentDidMount(){
+    const user = auth.currentUser;
+    if(user){
+      this.setState({ email: user.email });
+
+      db.collection('users')
+        .where('owner', '==', user.email)
+        .onSnapshot((docs) => {
+          docs.forEach(doc => {
+            this.setState({ username: doc.data().username })
+          })
+        })
+    }
+    
+
+    }
+    logout(){
+    auth.signOut()
+    .then(()=> this.props.navigation.navigate('Registro'))
+    .catch(err=> console.log('err', err))
+    }
+
+    render(){
+        return (
         <View>
-        <Text> Formulario de Profile </Text>
-        <Pressable onPress={()=> this.props.navigation.navigate('Login')}> 
+        <Text>Tu Perfil</Text>
+        <View>
+          <Text>Usuario: {this.state.username}</Text>
+          <Text>Email: {this.state.email}</Text>
+        </View>
+        <Text>Tus posteos: </Text>
+        <Pressable onPress={()=> this.logout('Login')}> 
         <Text>Cerrar Sesion</Text> </Pressable>
         </View>
-    )
+    )}
 }
 
 export default Profile;
